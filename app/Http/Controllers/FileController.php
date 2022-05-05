da3138bef7fee2377c9f33ac8484902ae816d32a
Miles Wu <slion0725@gmail.com>
2022-05-05 17:37:12 +0800

    upd

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\FileRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
@@ -15,8 +16,34 @@ class FileController extends Controller
      */
     public function file(FileRequest $request): JsonResponse
     {
+        $this->removeExpiredFiles();
+
         $fileName = basename($request->file('file')->store('temporary'));
 
         return response()->json(['file' => $fileName], 200);
+    }
+
+    private function removeExpiredFiles()
+    {
+        $path = config('filesystems.disks.temporary.root');
+
+        if (is_dir($path)) {
+            $dh = opendir($path);
+
+            if ($dh) {
+                while (false !== ($file = readdir($dh))) {
+                    if (is_file($path.'/'.$file)) {
+                        $time = filemtime($path.'/'.$file);
+
+                        if ((time() - $time) > 24*3600) {
+                            // unlink($path.'/'.$file);
+                            Storage::disk('temporary')->delete($file);
+                        }
+                    }
+                }
+
+                closedir($dh);
+            }
+        }
     }
 }
